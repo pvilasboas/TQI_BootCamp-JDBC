e322a187d9ed9da2dec0d4ebe66595b041316a04
Pedro Roberto <pvilasboas@gmail.com>
2022-06-01 19:08:37 -0300

    Fixed the gradle.build implementation group instead of compile - End of implementation of part 1 JDBC connectivity test

--- src/main/java/part1/ConnectionJDBC.java
+++ src/main/java/part1/ConnectionJDBC.java
@@ -1,8 +1,32 @@
 package part1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionJDBC {
 
     public static void main(String args[]){
+
+        String driver = "mysql";
+        String dataBaseAddress = "localhost";
+        String dataBaseName = "digital_innovation_one";
+        String user = "root";
+        String password = "password";
+
+        StringBuilder sb = new StringBuilder("jdbc:")
+                .append(driver).append("://")
+                .append(dataBaseAddress).append("/")
+                .append(dataBaseName);
+
+        String connectionUrl = sb.toString();
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, password)) {
+            System.out.println("sucesso!");
+        } catch (SQLException e){
+            System.out.println("falha");
+            e.printStackTrace();
+        }
 
     }
 
