f0ac4a1fdb1552cc88052fee35ed2502b0019edd
Pedro Roberto <pvilasboas@gmail.com>
2022-06-02 10:57:55 -0300

    added ConnectionFactory class

--- src/main/java/part2/ConnectionFactory.java
+++ src/main/java/part2/ConnectionFactory.java
@@ -1,6 +1,11 @@
 package part2;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory{
 
@@ -12,7 +17,35 @@ public class ConnectionFactory{
 
         Connection connection = null;
 
+        try (InputStream input = ConnectionFactory.class.getClassLoader()
+                .getResourceAsStream("connection.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
 
+            String driver = prop.getProperty("jdbc.driver");
+            String dataBaseAddress = prop.getProperty("db.address");
+            String dataBaseName = prop.getProperty("db.name");
+            String user = prop.getProperty("db.user.login");
+            String password = prop.getProperty("db.user.password");
+
+            StringBuilder sb = new StringBuilder("jdbc:")
+                    .append(driver).append("://")
+                    .append(dataBaseAddress).append("/")
+                    .append(dataBaseName);
+
+            String connectionUrl = sb.toString();
+
+            try {
+                connection = DriverManager.getConnection(connectionUrl, user, password);
+            } catch (SQLException e) {
+                System.out.println("Falha ao criar conexao");
+                throw new RuntimeException(e);
+            }
+
+        }catch (IOException e) {
+            System.out.println("falha ao carregar arquivos de propriedaes");
+            e.printStackTrace();
+        }
 
         return connection;
     }
