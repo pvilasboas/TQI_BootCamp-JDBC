2e9fbe322ca0cfed04b87a0ea266bc4872febf52
Pedro Roberto <pvilasboas@gmail.com>
2022-06-02 11:05:31 -0300

    added class AlunoDAO and the method list to list all alunos records

--- src/main/java/part2/AlunoDAO.java
+++ src/main/java/part2/AlunoDAO.java
@@ -1,5 +1,45 @@
 package part2;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlunoDAO {
+
+    //lista todos os alunos cadastrados no banco
+    public List<Aluno> list(){
+
+        List<Aluno> alunos = new ArrayList<>();
+
+        try (Connection conn = ConnectionFactory.getConnection()){
+            String sql = "SELECT * FROM aluno";
+
+            PreparedStatement stmt = conn.prepareCall(sql);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                int idade = rs.getInt("idade");
+                String estado = rs.getString("estado");
+
+                alunos.add(new Aluno(
+                        id,
+                        nome,
+                        idade,
+                        estado
+                ));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("listagem de alunos falhou");
+            e.printStackTrace();
+        }
+        return alunos;
+    }
 
 }
