92bfc2d296d9d683e90abfa8a9ed7e0ad47156db
Pedro Roberto <pvilasboas@gmail.com>
2022-06-02 11:37:20 -0300

    added method consult by id into QueriesExecution class

--- src/main/java/part2/AlunoDAO.java
+++ src/main/java/part2/AlunoDAO.java
@@ -9,6 +9,30 @@ import java.util.List;
 
 public class AlunoDAO {
 
+    public static Aluno getById(int id) {
+
+        Aluno aluno = new Aluno();
+        try (Connection conn = ConnectionFactory.getConnection()){
+            String sql = "SELECT * FROM aluno WHERE id = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()){
+                aluno.setId(rs.getInt("id"));
+                aluno.setNome(rs.getString("nome"));;
+                aluno.setIdade(rs.getInt("idade"));
+                aluno.setEstado(rs.getString("estado"));
+            }
+
+        } catch (SQLException e){
+            System.out.println("listagem falhou");
+            e.printStackTrace();
+        }
+
+        return aluno;
+    }
+
     //lista todos os alunos cadastrados no banco
     public List<Aluno> list(){
 
--- src/main/java/part2/QueriesExecution.java
+++ src/main/java/part2/QueriesExecution.java
@@ -8,9 +8,17 @@ public class QueriesExecution {
 
         AlunoDAO alunoDAO = new AlunoDAO();
 
+        //Consulta todos os alunos
         List<Aluno> alunos = alunoDAO.list();
-
+        System.out.println("Retorno de todos os registros:");
         alunos.stream().forEach(System.out::println);
+
+        //consulta aluno por id
+        Aluno alunoParaConsulta = AlunoDAO.getById(3);
+        System.out.println("Retorno da consulta por ID:");
+        System.out.println(alunoParaConsulta);
+
+
 
     }
 }
